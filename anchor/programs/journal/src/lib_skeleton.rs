53ca86cd95299b581b8208c116bfba29f858ef04
mungyo7 <mungyo7@yonsei.ac.kr>
2025-06-04 15:17:25 +0900

    skelton 수정

--- anchor/programs/journal/src/lib_skeleton.rs
+++ anchor/programs/journal/src/lib_skeleton.rs
@@ -3,6 +3,8 @@ use anchor_lang::prelude::*;
 // This is your program's public key and it will update
 // automatically when you build the project.
 declare_id!("");
+
+// Program Account -> 코드
 #[program]
 mod journal {
     use super::*;
@@ -12,6 +14,8 @@ mod journal {
         msg!("Title: {}", title);
         msg!("Message: {}", message);
 
+        let journal_entry = &mut ctx.accounts.journal_entry;
+
         Ok(())
     }
 
@@ -19,6 +23,8 @@ mod journal {
         msg!("Journal Entry Updated");
         msg!("Title: {}", title);
         msg!("Message: {}", message);
+
+        let journal_entry = &mut ctx.accounts.journal_entry;
 
         Ok(())
     }
@@ -29,17 +35,49 @@ mod journal {
     }
 }
 
+// Account 구조(붕어빵 틀)
 #[account]
 pub struct JournalEntryState {}
 
+// Account 사용 설명서
 #[derive(Accounts)]
 #[instruction(title: String, message: String)]
-pub struct CreateEntry<'info> {}
+pub struct CreateEntry<'info> {
+    #[account(
+
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
 #[instruction(title: String, message: String)]
-pub struct UpdateEntry<'info> {}
+pub struct UpdateEntry<'info> {
+    #[account(
+
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
 #[instruction(title: String)]
-pub struct DeleteEntry<'info> {}
+pub struct DeleteEntry<'info> {
+    #[account(
+
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
